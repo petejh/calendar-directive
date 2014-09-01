d0f2a6c625742391b5f6675df6d9b3c1349ae209
petejh <petejh0@users.noreply.github.com>
2014-09-01 14:48:08 -0700

    Add controls to select the month and year
    The calendar grid and the controls are not linked at this point.

--- app/app.js
+++ app/app.js
@@ -9,10 +9,38 @@ angular.module('CalendarDirectiveChallenge', [])
         scope.calendar = {
           range: CalendarRange.getMonthlyRange(new Date()),
           isInCurrentMonth: function(day) {
-            var today = new Date();
-            return (day.month == today.getMonth());
+            var currentMonth = new Date().getMonth();
+            return (day.month == currentMonth);
           }
         };
+      }
+    };
+  })
+
+  .directive('datepicker', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'datepicker-template.html',
+      scope: true,
+      link: function(scope, element, attrs) {
+
+        function setYears() {
+          var years = [];
+          var currentYear = new Date().getFullYear();
+          for (i = currentYear - 20; i <= currentYear + 20; i++) {
+            years.push(i);
+          }
+          return years;
+        }
+
+        scope.range = {
+          months: [
+          'January', 'February', 'March', 'April', 'May', 'June',
+          'July', 'August', 'September', 'October', 'November', 'December'
+          ],
+          years: setYears()
+        };
+
       }
     };
   });
