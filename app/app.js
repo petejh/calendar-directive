194f8c5c2d1279a47a6bd1b92a9711e6ef86bdfb
petejh <petejh0@users.noreply.github.com>
2014-09-03 23:20:21 -0700

    Wire the calendar and date picker directives together
    Also fix the date range calculation for March, which had been returning
    one less day than it should.

--- app/app.js
+++ app/app.js
@@ -3,15 +3,23 @@ angular.module('CalendarDirectiveChallenge', [])
   .directive('calendar', function() {
     return {
       restrict: 'E',
+      scope: {
+        date: '=' // <calendar date="sharedvariable"></calendar>
+      },
       templateUrl: 'calendar-template.html',
-      scope: true,
       link: function(scope, element, attrs) {
-        scope.calendar = {
-          range: CalendarRange.getMonthlyRange(new Date()),
-          isInCurrentMonth: function(day) {
-            var currentMonth = new Date().getMonth();
-            return (day.month == currentMonth);
-          }
+
+        var currentDate = new Date();
+
+        scope.$watchCollection('date', function(date) {
+          currentDate = new Date(date.year, date.month, date.day);
+          scope.days = CalendarRange.getMonthlyRange(currentDate).days;
+        });
+
+        scope.days = CalendarRange.getMonthlyRange(currentDate).days;
+
+        scope.isInCurrentMonth = function(day) {
+          return (day.month == currentDate.getMonth());
         };
       }
     };
@@ -20,27 +28,38 @@ angular.module('CalendarDirectiveChallenge', [])
   .directive('datepicker', function() {
     return {
       restrict: 'E',
+      scope: {
+        date: '=' // <datepicker date="sharedvariable"></datepicker>
+      },
       templateUrl: 'datepicker-template.html',
-      scope: true,
       link: function(scope, element, attrs) {
 
+        var today = new Date();
+        var yearsToSelect = 20;
+
         function setYears() {
-          var years = [];
-          var currentYear = new Date().getFullYear();
-          for (i = currentYear - 20; i <= currentYear + 20; i++) {
-            years.push(i);
+          var yearsRange = [];
+          var currentYear = today.getFullYear();
+          for (i = currentYear - yearsToSelect;
+               i <= currentYear + yearsToSelect;
+               i++) {
+            yearsRange.push(i);
           }
-          return years;
+          return yearsRange;
         }
 
-        scope.range = {
-          months: [
-          'January', 'February', 'March', 'April', 'May', 'June',
-          'July', 'August', 'September', 'October', 'November', 'December'
-          ],
-          years: setYears()
+        scope.date = scope.date || {
+          year: today.getFullYear(),
+          month: today.getMonth(),
+          day: 1
         };
 
+        scope.months = [
+          'January', 'February', 'March', 'April', 'May', 'June',
+          'July', 'August', 'September', 'October', 'November', 'December'
+        ];
+
+        scope.years = setYears();
       }
     };
   });
